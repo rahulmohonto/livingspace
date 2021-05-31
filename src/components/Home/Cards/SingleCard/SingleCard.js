9d899ca5709d746aa0ca4b291b39516d72d6e6fb
rahulmohonto <rahulmohonto1999@gmail.com>
2021-05-31 13:33:21 +0600

    card design change

--- src/components/Home/Cards/SingleCard/SingleCard.js
+++ src/components/Home/Cards/SingleCard/SingleCard.js
@@ -7,44 +7,38 @@ import { useHistory } from 'react-router-dom';
 import home from '../../../../images/home.png';
 import bed from '../../../../images/bed.png';
 
-
-
 const SingleCard = (props) => {
-    console.log(props);
-    const { apartmentName, image, apartmentPrice, type, _id, address, details } = props.data;
-
-    const SingleCard = (props) => {
-        const { apartmentName, id, size, monthlyRent, address, image, apartmentPrice, type } = props.data;
-        const history = useHistory()
-        const handleSelectApartment = _id => {
-            history.push(`/details/${_id}`)
-        }
+    const { apartmentName, _id, address, image, apartmentPrice, type } = props.data;
+    const history = useHistory()
+    const handleSelectApartment = _id => {
+        history.push(`/details/${_id}`)
+    }
 
 
-        return (
-            <Reveal duration={3000}>
+    return (
+        <Reveal duration={3000}>
 
-                <Card onClick={() => handleSelectApartment(_id)} style={{ width: '18rem', height: '25rem', marginBlock: '1rem' }} className="shadow">
-                    <Card.Img variant="top" src={image} style={{ height: '12rem' }} />
-                    <Card.Body>
-                        <Card.Title style={{ height: '2rem' }}>{apartmentName}</Card.Title>
-                        <Card.Text>
+            <Card onClick={() => handleSelectApartment(_id)} style={{ width: '18rem', height: '25rem', marginBlock: '1rem' }} className="shadow">
+                <Card.Img variant="top" src={image} style={{ height: '12rem' }} />
+                <Card.Body>
+                    <Card.Title style={{ height: '2rem' }}>{apartmentName}</Card.Title>
+                    <Card.Text>
 
-                            <img className="img-fluid homeIcon" src={home} alt="/" />   {address}<br />
-                            <img className="img-fluid homeIcon" src={bed} alt="/" />    {type}
+                        <img className="img-fluid homeIcon" src={home} alt="/" />   {address}<br />
+                        <img className="img-fluid homeIcon" src={bed} alt="/" />    {type}
 
-                        </Card.Text>
-                        <Card.Footer style={{ height: '3rem' }}>
+                    </Card.Text>
+                    <Card.Footer style={{ height: '3rem' }}>
 
-                            <h5 className="text-success priceText">{apartmentPrice}</h5>
+                        <h5 className="text-success priceText">{apartmentPrice}</h5>
 
-                        </Card.Footer>
-                    </Card.Body>
-                </Card>
+                    </Card.Footer>
+                </Card.Body>
+            </Card>
 
-            </Reveal >
+        </Reveal >
 
-        );
-    };
-}
+    );
+};
+
 export default SingleCard;
