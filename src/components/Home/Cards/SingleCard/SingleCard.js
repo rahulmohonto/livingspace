38d62bebd8e6216114bca12d71c4829fc490374b
rahulmohonto <rahulmohonto1999@gmail.com>
2021-05-31 13:19:20 +0600

    card update

--- src/components/Home/Cards/SingleCard/SingleCard.js
+++ src/components/Home/Cards/SingleCard/SingleCard.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 import Reveal from 'react-reveal/Reveal';
+import './SingleCard.css'
 import { useHistory } from 'react-router-dom';
+import home from '../../../../images/home.png';
+import bed from '../../../../images/bed.png';
+
+
+
 const SingleCard = (props) => {
     console.log(props);
-    const { apartmentName, id, image, apartmentPrice, type, _id } = props.data;
+    const { apartmentName, image, apartmentPrice, type, _id, address, details } = props.data;
 
     console.log(_id);
     const history = useHistory()
@@ -13,20 +19,30 @@ const SingleCard = (props) => {
         history.push(`/details/${_id}`)
     }
 
+
     return (
         <Reveal duration={3000}>
-            <Card onClick={() => handleSelectApartment(_id)} style={{ width: '18rem', marginBlock: '1rem' }}>
-                <Card.Img variant="top" src={props.data.image} />
+
+            <Card onClick={() => handleSelectApartment(_id)} style={{ width: '18rem', height: '25rem', marginBlock: '1rem' }} className="shadow">
+                <Card.Img variant="top" src={image} style={{ height: '12rem' }} />
                 <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
+                    <Card.Title style={{ height: '2rem' }}>{apartmentName}</Card.Title>
                     <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                </Card.Text>
-                    <Button variant="warning">Go somewhere</Button>
+
+                        <img className="img-fluid homeIcon" src={home} alt="/" />   {address}<br />
+                        <img className="img-fluid homeIcon" src={bed} alt="/" />    {type}
+
+                    </Card.Text>
+                    <Card.Footer style={{ height: '3rem' }}>
+
+
+                        <h5 className="text-success priceText">{apartmentPrice}</h5>
+
+                    </Card.Footer>
                 </Card.Body>
             </Card>
-        </Reveal>
+
+        </Reveal >
 
     );
 };
